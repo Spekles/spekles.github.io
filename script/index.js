8b59e007a3d97b8416c045790dab51e0c4de3962
Spekles <spekles118@gmail.com>
2020-11-09 16:14:56 +0200

    first content

--- script/index.js
+++ script/index.js
@@ -1,4 +1,2 @@
 const $like = document.querySelector("#like");
-if ($like instanceof HTMLElement) {
-	$like.addEventListener("click", () => ($like.style.color = "red"));
-}
+$like.addEventListener("click", () => ($like.style.color = "red"));
