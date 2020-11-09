12e7f00a22df55748d1883099d7c0c24b7c04ae3
Spekles <47836294+Spekles@users.noreply.github.com>
2020-11-09 17:59:19 +0200

    Delete index.js

--- script/index.js
+++
@@ -1,5 +0,0 @@
-const $like = document.querySelector("#like");
-$like.addEventListener("click", () => ($like.style.color = "red"));
-document.addEventListener('DOMContentLoaded', (event) => {
-console.log('DOM fully loaded and parsed');
-});
